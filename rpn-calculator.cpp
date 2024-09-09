fc19cc690bce54e4918090113e1b3bd4e39cadeb
Brendan Bobryk <babobryk@gmail.com>
2024-09-08 17:35:23 -0700

    Implemented cmd_or switch case

--- rpn-calculator.cpp
+++ rpn-calculator.cpp
@@ -153,7 +153,28 @@ shared_ptr<uint16_t> rpn_calc(command const cmd, uint16_t const value = 0)
         }
         break;
     case cmd_or:
-        // Perform bitwise OR operation
+        // If the stack has fewer than 2 elements, return nullptr
+        if (rpn_stack.size() < 2)
+        {
+            return nullptr;
+        }
+        else
+        {
+            // Pop two values from the stack
+            uint16_t a = rpn_stack.top();
+            rpn_stack.pop();
+            uint16_t b = rpn_stack.top();
+            rpn_stack.pop();
+
+            // Perform the bitwise OR operation
+            uint16_t result = a | b;
+
+            // Push the result back onto the stack
+            rpn_stack.push(result);
+
+            // Return a pointer to the top of the stack
+            return make_shared<uint16_t>(rpn_stack.top());
+        }
         break;
     case cmd_and:
         // Perform bitwise AND operation
